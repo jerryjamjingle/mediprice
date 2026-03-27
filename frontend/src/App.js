6ef5028ea861486f6341a8733115e50435b1e92e
jerryjamjingle <jerryjamjingle@gmail.com>
2026-03-26 22:53:54 -0500

    Fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Home';
 import Search from './Search';
